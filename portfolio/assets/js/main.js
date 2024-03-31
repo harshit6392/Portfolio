e9222516aa67f7a4cc2e9cb7b6b0b501ab1c9be9
Harshit <hsharma6392@gmail.com>
2024-03-31 17:12:38 +0530

    Feat: Scroll behaviour added.

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,3 +1,15 @@
+document.querySelectorAll('a[data-scroll]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
+
 !(function($) {
   "use strict";
 
